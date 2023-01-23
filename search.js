e1b7d7b4aea0086343e5cec0956dbfc274a0afdb
Gamer Tag <kodanadiarra456@gmail.com>
2023-01-23 09:50:16 +0000

    Add XMLHttpRequest and reformat search.js

--- search.js
+++ search.js
@@ -1,116 +1,77 @@
-$(function () {
+const XHRequest = new XMLHttpRequest();
+let sourceSearch = [];
 
+XHRequest.open('GET', 'https://jsonplaceholder.typicode.com/users');
+XHRequest.send();
 
-    $('#searchInput').on('change paste keyup', function () {
+XHRequest.responseType = 'json';
 
-        console.log('search input a changé : ' + $(this).val());
+XHRequest.onload = () => {
+    if (XHRequest.readyState === 4 && XHRequest.status === 200) {
+        const response = XHRequest.response;
 
-        if ($(this).val() == '') {
-            $('#jeux').html('');
-        }
-        /**
-         * Si la value est > à 3 chars, on recherche
-         */
-
-        if ( $(this).val().length >= 3 ) {
-
-            $.get(
-                'http://localhost/MVC-course/api/jeux/search/' + $(this).val(),
-                false,
-                function (response) {
-
-                    console.log(response);
-
-                    $('#jeux').html('');
-
-                    response.forEach(jeu => {
-
-                        $('#jeux').append('<div class="card"><div class="card-body">' + jeu.jeux_id + ': ' + jeu.jeux_nom + '</div></div>');
-
-                    });
-                },
-                'json'
-            );
+        // getting all name from api to a list of search
+        for (let i = 0; i < response.length; i++) {
+            sourceSearch.push(response[i].name);
         }
 
-    });
+        $('#search').click('input',function(e)
+        {
+            searchString($(this).val(), sourceSearch);
+        });
 
+        $('#search').on('input',function(e)
+        {
+            searchString($(this).val(), sourceSearch);
+        });
+
+        $('#search').blur('input',function(e)
+        {
+            removeSearchOutput();
+        });
+
+    } else {
+        console.error("Error: ", XHRequest.status);
+    }
+}
+
+let formParent = document.getElementById("search-parent");
+let firstChild = document.getElementById("search");
+
+function searchString(str, sourceSearch) {
+    removeSearchOutput();
+    let searchOutput = document.createElement('ul');
+    searchOutput.id = 'search-output';
+    searchOutput.classList.add('list-group');
+
+    let index;
+
+    for (let i=0; i < sourceSearch.length; i++) {
+        name = sourceSearch[i];
+        url = "https://kinsta.com/fr/blog/erreur-405-method-not-allowed/" // months[i].getAttribute('urltarget');
+
+        index = name.toLowerCase().indexOf(str.toLowerCase())
+
+        if (index !== -1) {
+            newElement = document.createElement('a');
+            newElement.textContent = name
+            newElement.classList.add('list-group-item');
+            newElement.classList.add('list-group-item-action');
+            //element = '<a class="list-group-item list-group-item-action" onclick=redirectTo('+'"'+url+'"'+')>' + name + '</a>'
+            newElement.href = url;
+            
+            searchOutput.appendChild(newElement)
+        }
+    }
+
+    formParent.appendChild(searchOutput);
+}
+
+function removeSearchOutput() {
     
-
-
-
-});
-
-
-
-/*
-$(function() {
-    $.get(
-        'http://localhost/videoclub/api/movies/',
-        false,
-        function (res) {
-            $('#films').html('');
-            res.forEach(movie => {
-                    var card =
-                        '<div class="card">' +
-                        '<div class="card-body">' +
-                        movie.id + ': ' + movie.title +
-                        '</div>' +
-                        '</div>';
-                    $('#films').append(card);
-            })
-        },
-        'json'
-    );
-    $.get(
-        'http://localhost/videoclub/api/categories/',
-        false,
-        function(res) {
-            res.forEach(category => {
-                console.log(category);
-                $('#selectCategories').append('<option value="'+ category.id +'">'+ category.title +'</option>');
-            });
-        },
-        'json'
-    );
-    $('#selectCategories').on('change', function() {
-        console.log('la categorie a été changée');
-        $.get(
-            'http://localhost/videoclub/api/movies/category/'+$(this).val(),
-            false,
-            function(res) {
-                $('#films').html('');
-                res.forEach(movie => {
-                    var card =
-                        '<div class="card">' +
-                        '<div class="card-body">' +
-                        movie.id + ': ' + movie.title +
-                        '</div>' +
-                        '</div>';
-                    $('#films').append(card);
-                });
-            },
-            'json'
-        );
-    });
-    $('#todos-load-btn').on('click', function () {
-        console.log('Le bouton a été cliqué');
-        $.get(
-            'http://localhost/videoclub/api/categories',
-            false,
-            function(res) {
-                res.forEach(r => {
-                    console.log(res)
-                    var card =
-                        '<div class="card">' +
-                        '<div class="card-body">' +
-                        r.id + ': ' + r.title +
-                        '</div>' +
-                        '</div>';
-                    $('#todolist').append(card);
-                });
-            },
-            'json'
-        );
-    });
-}) */
+    try {
+        document.getElementById('search-output').remove();
+    } catch (e) {
+    
+    };
+}
